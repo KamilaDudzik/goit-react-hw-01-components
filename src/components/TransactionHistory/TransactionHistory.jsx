00427c06d08555edbbbecae535b75ba23ad10323
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-07-18 19:34:28 +0200

    Added styles

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,31 +2,28 @@ import PropTypes from "prop-types";
 import css from "./Transactions.module.css";
 
 export const TransactionHistory = ({ items }) => (
-
+ 
     <table className={css.transactionHistory}>
-        <thead>
-            <tr className={css.categogoryRow}>
-                <th className={css.categoryItem}>Type</th>
-                <th className={css.categoryItem}>Amount</th>
-                <th className={css.categoryItem}>Currency</th>
-            </tr>
-        </thead>
-            
-        <tbody>
-            <tr>
-                <td>Invoice</td>
-                <td>125</td>
-                <td>USD</td>
-            </tr>
-            <tr>
-                <td>Withdrawal</td>
-                <td>85</td>
-                <td>USD</td>
-            </tr>
-        </tbody>
-    </table>
+      <thead>
+        <tr>
+          <th className={css.categoryRow}>Type</th>
+          <th className={css.categoryRow}>Amount</th>
+          <th className={css.categoryRow}>Currency</th>
+        </tr>
+      </thead>
 
-)
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id} className={css.tableList}>
+            <td className={css.categoryItem}>{type}</td>
+            <td className={css.categoryItem}>{amount}</td>
+            <td className={css.categoryItem}>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+ 
+);
 
 TransactionHistory.propTypes = {
     items: PropTypes.array,
@@ -34,4 +31,4 @@ TransactionHistory.propTypes = {
     type: PropTypes.string,
     amount: PropTypes.string,
     currency: PropTypes.string
-}
+};
